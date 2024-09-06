4ae4a7b4edcb830776903f3b267ee7120689c4f2
Oosh74 <usherjared@yahoo.com>
2024-09-05 23:27:23 -0400

    Practice using JavaScript to create DOM elements

---
+++ script.js
@@ -0,0 +1,24 @@
+const container = document.querySelector('#container');
+
+const redP = document.createElement('p');
+redP.setAttribute('style', 'color:red;');
+redP.textContent = "Hey I'm red!";
+
+const subTitle = document.createElement('h3');
+subTitle.setAttribute('style', 'color: blue');
+subTitle.textContent = "I'm a blue h3";
+
+const blackBox = document.createElement('div');
+blackBox.classList.add('black-box-style');
+
+const boxTitle = document.createElement('h1');
+boxTitle.textContent = "I'm inside the box!";
+
+const boxText = document.createElement('p');
+boxText.textContent = 'Me too!';
+
+container.appendChild(redP);
+container.append(subTitle);
+container.appendChild(blackBox);
+blackBox.appendChild(boxTitle);
+blackBox.appendChild(boxText);
