7024c83f4d96702c420e418c2fa9842ecad45ab8
Oosh74 <usherjared@yahoo.com>
2024-09-15 10:27:37 -0400

    creating simple form

--- script.js
+++ script.js
@@ -1,70 +1,81 @@
-const container = document.querySelector('#container');
+// const container = document.querySelector('#container');
 
-const redP = document.createElement('p');
-redP.setAttribute('style', 'color:red;');
-redP.textContent = "Hey I'm red!";
+// const redP = document.createElement('p');
+// redP.setAttribute('style', 'color:red;');
+// redP.textContent = "Hey I'm red!";
 
-const subTitle = document.createElement('h3');
-subTitle.setAttribute('style', 'color: blue');
-subTitle.textContent = "I'm a blue h3";
+// const subTitle = document.createElement('h3');
+// subTitle.setAttribute('style', 'color: blue');
+// subTitle.textContent = "I'm a blue h3";
 
-const blackBox = document.createElement('div');
-blackBox.classList.add('black-box-style');
+// const blackBox = document.createElement('div');
+// blackBox.classList.add('black-box-style');
+// blackBox.style.backgroundColor = 'purple'; //element.style.property adds as inline CSS
 
-const boxTitle = document.createElement('h1');
-boxTitle.textContent = "I'm inside the box!";
+// const boxTitle = document.createElement('h1');
+// boxTitle.textContent = "I'm inside the box!";
 
-const boxText = document.createElement('p');
-boxText.textContent = 'Me too!';
+// const boxText = document.createElement('p');
+// boxText.textContent = 'Me too!';
 
-container.appendChild(redP);
-container.append(subTitle);
-container.appendChild(blackBox);
-blackBox.appendChild(boxTitle);
-blackBox.appendChild(boxText);
+// container.appendChild(redP);
+// container.append(subTitle);
+// container.appendChild(blackBox);
+// blackBox.appendChild(boxTitle);
+// blackBox.appendChild(boxText);
 
-const btn = document.querySelector('.jsBtn');
-btn.onclick = () => {
-  alert('Hello, world!');
-};
+// const btn = document.querySelector('.jsBtn');
+// btn.onclick = () => {
+//   alert('Hello, world!');
+// };
 
-const eventBtn = document.querySelector('.eventListenerBtn');
-eventBtn.addEventListener('click', () => {
-  alert('hello, world!');
+// const eventBtn = document.querySelector('.eventListenerBtn');
+// eventBtn.addEventListener('click', () => {
+//   alert('hello, world!');
+// });
+
+// eventBtn.addEventListener('click', (e) => {
+//   e.target.setAttribute('style', 'background-color: blue; color: white'); //setAttribute does not add as inline CSS
+// });
+
+// const buttons = document.querySelectorAll('button');
+
+// buttons.forEach((button) => {
+//   button.addEventListener('click', () => {
+//     alert('Clicked');
+//   });
+// });
+
+// const link = document.querySelector('a');
+// link.textContent = 'Mozzila Developer Network';
+// link.href = 'https://developer.mozilla.org';
+// link.target = '_blank';
+
+// link.addEventListener('mouseover', () => {
+//   link.style.cursor = 'pointer';
+//   link.style.fontSize = '50px';
+// });
+
+// link.addEventListener('mouseleave', () => {
+//   link.style.fontSize = '24px';
+// });
+
+// const sect = document.querySelector('section');
+// const sectText = document.createElement('p');
+// sectText.textContent = 'I was made from javascript';
+// sect.appendChild(sectText);
+// const moreText = document.createTextNode(
+//   ' - the best source for web dev knowledge'
+// );
+
+// sectText.appendChild(moreText);
+
+//Shopping List
+const list = document.querySelector('.shopping-list-ul');
+const listForm = document.querySelector('#shopping-form');
+const listBtn = document.querySelector('.shopping-list-btn');
+
+listBtn.addEventListener('click', () => {
+  console.log(listForm.value);
+  list.appendChild();
 });
-
-eventBtn.addEventListener('click', (e) => {
-  e.target.setAttribute('style', 'background-color: blue; color: white');
-});
-
-const buttons = document.querySelectorAll('button');
-
-buttons.forEach((button) => {
-  button.addEventListener('click', () => {
-    alert('Clicked');
-  });
-});
-
-const link = document.querySelector('a');
-link.textContent = 'Mozzila Developer Network';
-link.href = 'https://developer.mozilla.org';
-link.target = '_blank';
-
-link.addEventListener('mouseover', () => {
-  link.style.cursor = 'pointer';
-  link.style.fontSize = '50px';
-});
-
-link.addEventListener('mouseleave', () => {
-  link.style.fontSize = '24px';
-});
-
-const sect = document.querySelector('section');
-const sectText = document.createElement('p');
-sectText.textContent = 'I was made from javascript';
-sect.appendChild(sectText);
-const moreText = document.createTextNode(
-  ' - the best source for web dev knowledge'
-);
-
-sectText.appendChild(moreText);
